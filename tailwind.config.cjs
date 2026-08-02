ecdaaaaf52c5ff5e2131f6fc4b09a3b64eafdb54
Perkunz <113631468+Perkunz@users.noreply.github.com>
2026-08-02 12:22:22 +0100

    chore: add Tailwind config (tailwind.config.cjs)

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx}',
+    './components/**/*.js',
+    './*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
